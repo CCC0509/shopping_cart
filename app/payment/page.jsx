9189a671e1bce4fdb6c385dcffa0437364387e48
周家頡 <jackchou@zhoujiajiadeMacBook-Pro.local>
2024-04-29 00:33:30 +0800

    version 1.2.2 setting payment page redirect

--- app/payment/page.jsx
+++ app/payment/page.jsx
@@ -1,13 +1,21 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 import { useFn } from "@/context/cart-data-context";
 import OrderCheck from "@/components/payment-page/order-check";
 import OrderConfirm from "@/components/payment-page/order-confirm";
 
 import style from "./page.module.css";
+import { useEffect } from "react";
 
 const Payment = () => {
-  const { order } = useFn();
+  const { order, cartData } = useFn();
+  const router = useRouter();
+  useEffect(() => {
+    if (cartData.length === 0 && !order) {
+      return router.push("/");
+    }
+  }, [order, cartData]);
 
   return (
     <main className={style.container}>
